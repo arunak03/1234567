7b4e97a9622886821484b8ae1f28f990eec1484a
arunak03 <97444853+arunak03@users.noreply.github.com>
2023-10-19 20:52:28 +0530

    Add files via upload

---
+++ grocery store/demo.js
@@ -0,0 +1,17 @@
+function showSegment(segmentId, linkId) {
+    const segments = document.querySelectorAll('.segment');
+    segments.forEach(segment => {
+        segment.style.display = 'none';
+    });
+
+    const selectedSegment = document.getElementById(segmentId);
+    selectedSegment.style.display = 'block';
+
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
+        link.classList.remove('active-link');
+    });
+    
+    const selectedLink = document.getElementById(linkId);
+    selectedLink.classList.add('active-link');
+}
